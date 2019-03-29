bdb62042e7ab96333ec11fd4eb7f4e71d9e88d34
DonMahallem <donmahallem@users.noreply.github.com>
2019-03-29 08:25:43 +0100

    additional tests

--- src/promise-to-response.spec.ts
+++ src/promise-to-response.spec.ts
@@ -3,7 +3,14 @@ import * as express from "express";
 import "mocha";
 import * as sinon from "sinon";
 import { promiseToResponse } from "./promise-to-response";
-
+import * as reqp from 'request-promise-native';
+import { ServerResponse, IncomingMessage } from 'http';
+import * as request from 'request';
+interface ITestObject {
+    prom: Promise<any>,
+    nextSpyCallCount: number;
+    statusSpyCallCount: number;
+};
 describe("promise-to-response.ts", () => {
     describe("promiseToResponse(prom,res)", () => {
         let jsonSpy: sinon.SinonSpy;
@@ -29,53 +36,78 @@ describe("promise-to-response.ts", () => {
             nextSpy.resetHistory();
             statusStub.resetHistory();
         });
-        describe("next parameter provided", () => {
-            it("should forward the resolved value to the response", (done) => {
-                promiseToResponse(Promise.resolve(testResponse), resObj, nextSpy);
-                setTimeout(() => {
-                    expect(nextSpy.callCount).to.equal(0);
-                    expect(statusStub.callCount).to.equal(1);
-                    expect(jsonSpy.callCount).to.equal(1);
-                    expect(statusStub.getCall(0).args).to.deep.equal([200]);
-                    expect(jsonSpy.getCall(0).args).to.deep.equal([testResponse]);
-                    done();
-                }, 100);
-            });
-            it("should forward the error to the next function", (done) => {
-                promiseToResponse(Promise.reject(testResponse), resObj, nextSpy);
-                setTimeout(() => {
-                    expect(nextSpy.callCount).to.equal(1);
-                    expect(statusStub.callCount).to.equal(0);
-                    expect(jsonSpy.callCount).to.equal(0);
-                    expect(nextSpy.getCall(0).args).to.deep.equal([testResponse]);
-                    done();
-                }, 100);
+        describe('promise rejects', () => {
+
+        });
+        describe('promise resolves', () => {
+            [true, false].forEach((nextProvided) => {
+                describe("next parameter " + (nextProvided ? '' : 'not') + " provided", () => {
+                    it("should forward the resolved value to the response", (done) => {
+                        if (nextProvided)
+                            promiseToResponse(Promise.resolve(testResponse), resObj, nextSpy);
+                        else
+                            promiseToResponse(Promise.resolve(testResponse), resObj);
+                        setTimeout(() => {
+                            expect(nextSpy.callCount).to.equal(0);
+                            expect(statusStub.callCount).to.equal(1);
+                            expect(jsonSpy.callCount).to.equal(1);
+                            expect(statusStub.getCall(0).args).to.deep.equal([200]);
+                            expect(jsonSpy.getCall(0).args).to.deep.equal([testResponse]);
+                            done();
+                        }, 100);
+                    });
+                });
             });
         });
-        describe("next parameter not provided", () => {
-            it("should forward the resolved value to the response", (done) => {
-                promiseToResponse(Promise.resolve(testResponse), resObj);
-                setTimeout(() => {
-                    expect(nextSpy.callCount).to.equal(0);
-                    expect(statusStub.callCount).to.equal(1);
-                    expect(jsonSpy.callCount).to.equal(1);
-                    expect(statusStub.getCall(0).args).to.deep.equal([200]);
-                    expect(jsonSpy.getCall(0).args).to.deep.equal([testResponse]);
-                    done();
-                }, 100);
+        describe('promise rejects', () => {
+            const testErrors: any[] = [
+                {
+                    error: {
+                        statusCode: 400,
+                        message: 'another message'
+                    },
+                    response: {
+                        statusCode: 400
+                    }
+                },
+                {
+                    error: new Error('test erorr'),
+                    response: {
+                        statusCode: 500
+                    }
+                },
+            ]
+            describe("next parameter provided", () => {
+                testErrors.forEach((testError) => {
+                    it("should forward the error to the next function", (done) => {
+                        promiseToResponse(Promise.reject(testError.error), resObj, nextSpy);
+                        setTimeout(() => {
+                            expect(nextSpy.callCount).to.equal(1);
+                            expect(statusStub.callCount).to.equal(0);
+                            expect(jsonSpy.callCount).to.equal(0);
+                            expect(nextSpy.getCall(0).args).to.deep.equal([testError.error]);
+                            done();
+                        }, 100);
+                    });
+                });
             });
-            it("should send an error directly back", (done) => {
-                promiseToResponse(Promise.reject(testResponse), resObj);
-                setTimeout(() => {
-                    expect(nextSpy.callCount).to.equal(0);
-                    expect(statusStub.callCount).to.equal(1);
-                    expect(jsonSpy.callCount).to.equal(1);
-                    expect(statusStub.getCall(0).args).to.deep.equal([500]);
-                    expect(jsonSpy.getCall(0).args).to.deep.equal([{
-                        error: true,
-                    }]);
-                    done();
-                }, 100);
+            describe("next parameter not provided", () => {
+                testErrors.forEach((testError) => {
+                    it("should forward the error to the next function", (done) => {
+                        promiseToResponse(Promise.reject(testError.error), resObj);
+                        setTimeout(() => {
+                            expect(nextSpy.callCount).to.equal(0);
+                            expect(statusStub.callCount).to.equal(1);
+                            expect(jsonSpy.callCount).to.equal(1);
+                            expect(statusStub.getCall(0).args).to.deep.equal([testError.response.statusCode]);
+                            expect(jsonSpy.getCall(0).args).to.deep.equal([{
+                                error: true,
+                                statusCode: testError.response.statusCode,
+                            }]);
+                            done();
+                        }, 100);
+                    });
+                });
             });
         });
     });
--- src/promise-to-response.ts
+++ src/promise-to-response.ts
@@ -8,8 +8,8 @@ import { RequestPromise } from "request-promise-native";
  * @param next
  */
 export const promiseToResponse = <T>(prom: Promise<T> | RequestPromise<T>,
-                                     res: express.Response,
-                                     next?: express.NextFunction): void => {
+    res: express.Response,
+    next?: express.NextFunction): void => {
     prom
         .then((value: T) => {
             res.status(200).json(value);
@@ -18,8 +18,10 @@ export const promiseToResponse = <T>(prom: Promise<T> | RequestPromise<T>,
             if (next) {
                 next(err);
             } else {
-                res.status(500).json({
+                const code: number = err.statusCode ? err.statusCode : 500;
+                res.status(code).json({
                     error: true,
+                    statusCode: code,
                 });
             }
         });
