70450e9eae1724a6395b09d258e7c9a0703fc50a
donmahallem <donmahallem@users.noreply.github.com>
2019-09-21 08:58:56 +0200

    fix

--- .github/setup_npmjs_config.sh
+++ .github/setup_npmjs_config.sh
@@ -1,1 +1,2 @@
-"//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$HOME/.npmrc"
+#!/bin/sh
+printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$HOME/.npmrc"
