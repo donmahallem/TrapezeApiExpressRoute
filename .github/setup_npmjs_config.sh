9d29efca58069d37a35d2135c09b1b899acdc699
donmahallem <donmahallem@users.noreply.github.com>
2019-09-23 12:57:40 +0200

    removed

--- .github/setup_npmjs_config.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$HOME/.npmrc"
