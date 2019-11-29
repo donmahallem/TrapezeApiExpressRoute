eaaf36676901e9c25287de342efc4aa4e0d834e2
donmahallem <donmahallem@users.noreply.github.com>
2019-11-29 09:42:41 +0000

    Automated deployment: Fri Nov 29 09:42:41 UTC 2019 640771fe1b41312a58ce9aed7881c00d5bd91aec

--- api/api_project.js
+++ api/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "Trapeze Api Route Docs",
-  "version": "1.8.3",
+  "version": "1.8.4",
   "description": "",
   "title": "Trapeze Api Route",
   "sampleUrl": false,
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-11-28T20:08:45.399Z",
+    "time": "2019-11-29T09:42:29.012Z",
     "url": "http://apidocjs.com",
     "version": "0.18.0"
   }
