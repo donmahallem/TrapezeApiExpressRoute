6191318e84602fedf24689d728deed41a36b5f4b
donmahallem <donmahallem@users.noreply.github.com>
2020-01-14 17:39:16 +0100

    add tests for settings endpoint

---
+++ src/endpoints/settings.spec.ts
@@ -0,0 +1,81 @@
+/*!
+ * Source https://github.com/donmahallem/TrapezeApiExpressRoute
+ */
+
+import { TrapezeApiClient } from "@donmahallem/trapeze-api-client";
+import { expect } from "chai";
+import * as express from "express";
+import "mocha";
+import * as sinon from "sinon";
+import { ITestEndpoint } from "./common-test.spec";
+import { SettingsEndpoints } from "./settings";
+
+describe("endpoints/settings.ts", () => {
+    describe("SettingsEndpoints", () => {
+        describe("createSettingsEndpoint(client)", () => {
+            const methodStubResponse: any = {
+                method: true,
+                response: "test",
+                stub: 29,
+            };
+            const req: any = {
+                params: {
+                    id: 95482,
+                },
+            };
+            let stubInstance: sinon.SinonStubbedInstance<TrapezeApiClient>;
+            let sandbox: sinon.SinonSandbox;
+            let responseObj: any;
+            let nextObj: sinon.SinonStub;
+            let setHeaderStub: sinon.SinonStub;
+            let jsonStub: sinon.SinonStub;
+            let testRequestHandler: express.RequestHandler;
+            before(() => {
+                sandbox = sinon.createSandbox();
+                stubInstance = sandbox.createStubInstance(TrapezeApiClient);
+                nextObj = sandbox.stub();
+                setHeaderStub = sandbox.stub();
+                jsonStub = sandbox.stub();
+                responseObj = {
+                    json: jsonStub,
+                    setHeader: setHeaderStub,
+                };
+            });
+            beforeEach(() => {
+                testRequestHandler = SettingsEndpoints.createSettingsEndpoint(stubInstance as any);
+            });
+            afterEach("test and reset stubs", () => {
+                expect(stubInstance.getSettings.callCount).to.equal(1);
+                sandbox.reset();
+            });
+            after(() => {
+                sandbox.restore();
+            });
+            it("should pass on the provided parameters", () => {
+                stubInstance.getSettings.resolves(methodStubResponse);
+                return testRequestHandler(req, responseObj, nextObj)
+                    .then(() => {
+                        expect(stubInstance.getSettings.callCount).to.equal(1);
+                        expect(stubInstance.getSettings.getCall(0).args).to.deep.equal([]);
+                        expect(jsonStub.callCount).to.equal(1);
+                        expect(jsonStub.getCall(0).args)
+                            .to.deep.equal([methodStubResponse]);
+                        expect(setHeaderStub.callCount).to.equal(1);
+                        expect(setHeaderStub.getCall(0).args).to.deep
+                            .equal(["Cache-Control", "public, max-age=86400"]);
+                    });
+            });
+            it("should fail correctly inner methods correclty", () => {
+                stubInstance.getSettings.rejects(methodStubResponse);
+                return testRequestHandler(req, responseObj, nextObj)
+                    .then(() => {
+                        expect(stubInstance.getSettings.callCount).to.equal(1);
+                        expect(stubInstance.getSettings.getCall(0).args).to.deep.equal([]);
+                        expect(jsonStub.callCount).to.equal(0);
+                        expect(setHeaderStub.callCount).to.equal(0);
+                        expect(nextObj.calledOnceWithExactly(methodStubResponse)).to.equal(true);
+                    });
+            });
+        });
+    });
+});
